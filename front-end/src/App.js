6e25acad4e2367188ffdd94c1467439f30d4c063
Mitch <mitchsigley@icloud.com>
2019-09-20 15:43:05 +1000

    new charts, drop down

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './components/Dashboard';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div>
+      <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/components/CurrentState.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-script-url */
+
+import React from 'react';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Title from './Title';
+
+const useStyles = makeStyles({
+  depositContext: {
+    flex: 1,
+  },
+});
+
+export default function Deposits() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Title>Current State</Title>
+      
+      <Typography component="p" variant="h4">
+        Concentrating
+        <div className> </div>
+      </Typography>
+      
+    </React.Fragment>
+  );
+}
+
